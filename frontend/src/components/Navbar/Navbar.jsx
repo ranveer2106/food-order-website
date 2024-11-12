d383ce08e120b3eabc4d9752434096423894700b
ranvir <ranveerbhatti2106@gmail.com>
2024-11-12 13:21:59 +0530

    navbar final changes

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -5,23 +5,33 @@ import "./Navbar.css"
 
 const Navbar = () => {
 
-  const [first, setfirst] = useState(0);
+  const [page, setPage] = useState("menu");
   return (
     <>
       <div className='navbar flex justify-between border-b-4 px-12'>
         <a href="/" className='logocont pt-2'>
           <img src={assets.logo} alt="logo" className=' logo' />
         </a>
-        <ul className='flex items-center	'>
-          <li className=''><a href="/" className='btn'>Home</a> </li>
-          <li className=''><a href="/" className='btn'>Menu</a> </li>
-          <li className=''><a href="/" className='btn'> Contact Us</a></li>
+        <ul className='flex items-center	nav-main'>
+          <li onClick={()=>setPage("home")} className={`mx-2 navbtn ${page==="home"?"active":" "}` }>Home </li>
+          <li onClick={()=>setPage("menu")} className={`mx-2 navbtn ${page==="menu"?"active":" "}` }>Menu </li>
+          <li onClick={()=>setPage("contact us")} className={`mx-2 navbtn ${page==="contact us"?"active":" "}` }>Contact Us </li>
+
         </ul>
-        <div className='pt-4 flex'>
-          <a href='/' className='px-4 '><img className='w-8' src={assets.search} alt="/" /></a>
-          <a href="/" className='px-4 '><img className='w-8' src={assets.cart} alt="/" /></a>
-          <a href="/" className='px-4 py-0 '>Login</a>
-        </div>
+        <ul className='flex items-center '>
+          <li className='mx-4 righticons' ><ion-icon name="search-outline" size="large"></ion-icon></li>
+          <li className='cart-icon mx-4 righticons'><ion-icon name="cart-outline" size="large"></ion-icon>
+            <div className='cartdot'></div>
+          </li>
+          <li className='loginbtn mx-4'>login</li>
+          {/* <li><a href='/' className='mx-4 righticons'><ion-icon name="search-outline" size="large"></ion-icon></a></li> */}
+          {/* <li className='cart-icon'><a href="/" className='mx-4 righticons'><ion-icon name="cart-outline" size="large"></ion-icon></a>
+            <div className='cartdot'></div>
+          </li> */}
+          {/* <li><a href="/" className='mx-4 loginbtn'> Login </a></li> */}
+
+
+        </ul>
 
       </div>
     </>
