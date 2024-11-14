e2f0726f6c9bd7113bd8e965d8f1e6e32f896e6c
ranvir <ranveerbhatti2106@gmail.com>
2024-11-14 18:04:19 +0530

    Updated Footer component with new layout and added logo, text, and social icons

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import './Footer.css'
+import { assets } from '../../assets/assets'
 
 const Footer = () => {
   return (
     <>
         <div className='footer'>
-            Footer
+            <div className="footer-content">
+                <div className="footer-content-left">
+                    <img src={assets.logo} alt="logo"  className='w-40'/>
+                    <p>stuff</p>
+                    <div className="social-icons">
+                    <ion-icon name="logo-facebook"></ion-icon>
+                    <ion-icon name="logo-instagram"></ion-icon>
+                    <ion-icon name="logo-instagram"></ion-icon>
+                    <ion-icon name="logo-instagram"></ion-icon>
+                    </div>
+                </div>
+                <div className="footer-content-center">
+
+                </div>
+                <div className="footer-content-right">
+
+                </div>
+            </div>
         </div>
     </>
   )
