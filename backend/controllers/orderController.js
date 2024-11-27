f8fb78af8450402e858309e28fc169fb3c6dad2d
ranvir <ranveerbhatti2106@gmail.com>
2024-11-27 18:00:40 +0530

    change again

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -17,139 +17,49 @@ const instance = new Razorpay({
 
 
 const placeOrder = async (req,res) => {
-    const frontend_url = "http://localhost:5173"
-    // try {
-    //     const newOrder = new orderModel({
-    //         userId:req.body.userId,
-    //         items:req.body.items,
-    //         address:req.body.address,
 
-    //     })
-
-    //     await newOrder.save();
-    //     await userModel.findByIdAndUpdate(req.body.userId,{cartData:{}});
-
-    //     const line_items = req.body.items.map((item) => ({
-    //             price_data: {
-    //                 currency: "INR",
-    //                 product_data: {
-    //                     name: item.name,
-    //                 },
-    //                 unit_amount: item.price * 100, // Razorpay expects amount in paise (smallest unit)
-    //             },
-    //             quantity: item.quantity , // Ensure quantity is included, or default to 1 if not specified
-            
-    //     }));
-
-    //     line_items.push({
-    //         price_data:{
-    //             currency:"INR", 
-    //             product_data:{
-    //                 name: "Delivery Charges"
-    //             },
-    //             // 30rs delivery charges
-
-    //             unit_amount:30*100
-    //         },
-    //         quantity:1
-
-    //     })
-
-
-        
-
-    //     const session = await stripe.checkout.sessions.create({
-    //         line_items:line_items,
-    //         mode:paymentLink,
-    //         success_url:`${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-    //         cancel_url:`${frontend_url}/verify?success=false&orderId=${newOrder._id}`
-
-    //     })
-
-
-
-
-
-    // } 
+    // const amount = req.body.amount;
+    // const frontend_url = "http://localhost:5173";
 
 
     
     try {
+        
         const newOrder = new orderModel({
             userId:req.body.userId,
             items:req.body.items,
             amount:req.body.amount,
             address:req.body.address,
-           
+            
         })
-
+        
         await newOrder.save();
         await userModel.findByIdAndUpdate(req.body.userId,{cartData:{}});
-
-        const line_items = req.body.items.map((item) => ({
-                price_data: {
-                    currency: "INR",
-                    product_data: {
-                        name: item.name,
-                    },
-                    unit_amount: item.price * 100, // Razorpay expects amount in paise (smallest unit)
-                },
-                quantity: item.quantity , // Ensure quantity is included, or default to 1 if not specified
-            
-        }));
-
-        line_items.push({
-            price_data:{
-                currency:"INR", 
-                product_data:{
-                    name: "Delivery Charges"
-                },
-                // 30rs delivery charges
-
-                unit_amount:30*100
-            },
-            quantity:1
-
-        })
-
-
         
+        
+        
+        const options = {
+            amount: req.body.amount*100, // amount in smallest currency unit
+            currency: "INR",
+            receipt: "receipt_order_74394",
+        };
+        console.log("io");
 
-        // const session = await stripe.checkout.sessions.create({
-        //     line_items:line_items,
-        //     mode:paymentLink,
-        //     success_url:`${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-        //     cancel_url:`${frontend_url}/verify?success=false&orderId=${newOrder._id}`
-
-        // })
-        const totalAmountInPaise = line_items.reduce((total, item) => {
-          return total + (item.price_data.unit_amount * item.quantity);
-      }, 0);
-
-
-      const totalAmountInINR = totalAmountInPaise / 100;
-
-
-      const options = {
-        amount: totalAmountInPaise, // amount in smallest currency unit
-        currency: "INR",
-        receipt: "receipt_order_74394",
-    };
-
-
-    const order = await instance.orders.create(options);
-
-        if (!order) return res.status(500).send("Some error occured");
-
-        res.json(order);
-
-
-
+    razorpayInstance.orders.create(options, (error, order) => {
+        if (error) {
+            console.log(error);
+            return res.status(500).json({ message: "Something Went Wrong!" });
+        }
+        res.status(200).json({ data: order });
+        console.log(order)
+    });
 
 
     } 
     
     catch (error) {
+        res.status(500).json({ message: "Internal Server Error!" });
+
         console.log(error);
         
     }
